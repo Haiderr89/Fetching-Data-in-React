cf1c243d662b4f87b578b4f28ab9fc2de49096c9
Sayed Haider <sayedhaider2002@gmail.com>
2025-02-19 10:33:10 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 // src/App.jsx
+import './App.css'
+import * as studentService from './services/studentService'
+import { useState } from 'react'
+
 const App = () => {
 
+  const [students, setStudents] = useState([])
+
+  const fetchAllStudents = async () => {
+    const studentData = await studentService.index()
+    setStudents(studentData)
+  }
+
+  fetchAllStudents()
+
   return (
-    <h1>Hello world!</h1>
+    <>
+    {students.map((student) => (
+      <p>{student.name}</p>
+    ))}
+  
+    </>
   );
 }
 
---
+++ src/services/studentService.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://seb-2-pt-api.onrender.com'
+
+const index = async () => {
+    try {
+        const res = await fetch(`${BASE_URL}/students`)
+        const data = await res.json()
+        return data;
+        
+    } catch (error) {
+        console.log(error);   
+    }
+
+}
+
+export {
+    index,
+}
